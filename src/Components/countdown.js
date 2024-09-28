6a6aa6d969649bcb49d2824fbb18b67eb07813f1
heebrahymne <bankoleheebrahymn@gmail.com>
2024-09-28 10:20:54 +0100

    adding

--- src/Components/countdown.js
+++ src/Components/countdown.js
@@ -6,12 +6,12 @@ import Fresh from "./Countdown/fresh.png";
 import Rsnl from "./Countdown/rsnl.png";
 import VideoBg from  "./Countdown/videoBG.mp4";
 
-const COUNTDOWN_TARGET = new Date("2024-09-27T07:59:59");
+const COUNTDOWN_TARGET = new Date("2024-09-29T08:59:59");
 
 const getTimeLeft = () => {
-	// const totalTimeLeft = COUNTDOWN_TARGET - new Date();
+	const totalTimeLeft = COUNTDOWN_TARGET - new Date();
 	// const totalTimeLeft = 0;
-		const totalTimeLeft = new Date() - COUNTDOWN_TARGET;
+		// const totalTimeLeft = new Date() - COUNTDOWN_TARGET;
 
 	const DAYS = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
 	const HOURS = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
@@ -40,10 +40,10 @@ const Countdown = () => {
 			<div className='countdown'>
         <img src={Rsnl} alt="Rsnl Logo" width={300} height={60}/>
 		{/* <h3>Project Phoenix Countdown</h3> */}
-		<h3>Project Phoenix is Live!</h3>
-		<p>We are pleased to announce the successful cut over of Jara Stores at Ikeja, Ibadan, Oyo, Ado-Ekiti, Benin City (Ugbowo and Sakponba) and Shoprite Stores at The Palms, Lekki Mall, Circle Mall, Adeniran Ogunsanya, Ikeja City Mall, Festival Mall, Park Lane, Ota Gateway, Cocoa Mall, Ibadan Mall, Akure Mall, Kwara Mall, Grand Tower, Gateway, Jabilake Mall, Silverbird Mall, Galaxy Mall, Garden City Mall, Owerri Mall, Abia Mall, Enugu Mall, and Onitsha Mall to the new ERP. <div className='boldy' >Shoprite Onitsha Mall has been live for:</div> </p>
-		{/* <p>We are pleased to announce the successful cut over of Jara Stores at Ikeja, Ibadan, Oyo, Ado-Ekiti, Benin City (Ugbowo and Sakponba) and Shoprite Stores at The Palms, Lekki Mall, Circle Mall, Adeniran Ogunsanya, Ikeja City Mall, Festival Mall, Park Lane, Ota Gateway, Cocoa Mall, Ibadan Mall, Akure Mall, Kwara Mall, Grand Tower Mall, Gateway Apo, Jabilake Mall, Silverbird Mall, Galaxy Mall, Garden City Mall, Owerri Mall, Abia Mall, Enugu Mall to the new ERP.
-	     <div className='boldy' > Shoprite Onitsha Mall is scheduled to go live in:</div></p> */}
+		{/* <h3>Project Phoenix is Live!</h3>
+		<p>We are pleased to announce the successful cut over of Jara Stores at Ikeja, Ibadan, Oyo, Ado-Ekiti, Benin City (Ugbowo and Sakponba) and Shoprite Stores at The Palms, Lekki Mall, Circle Mall, Adeniran Ogunsanya, Ikeja City Mall, Festival Mall, Park Lane, Ota Gateway, Cocoa Mall, Ibadan Mall, Akure Mall, Kwara Mall, Grand Tower, Gateway, Jabilake Mall, Silverbird Mall, Galaxy Mall, Garden City Mall, Owerri Mall, Abia Mall, Enugu Mall, and Onitsha Mall to the new ERP. <div className='boldy' >Shoprite Onitsha Mall has been live for:</div> </p> */}
+		<p>We are pleased to announce the successful cut over of Jara Stores at Ikeja, Ibadan, Oyo, Ado-Ekiti, Benin City (Ugbowo and Sakponba) and Shoprite Stores at The Palms, Lekki Mall, Circle Mall, Adeniran Ogunsanya, Ikeja City Mall, Festival Mall, Park Lane, Ota Gateway, Cocoa Mall, Ibadan Mall, Akure Mall, Kwara Mall, Grand Tower Mall, Gateway Apo, Jabilake Mall, Silverbird Mall, Galaxy Mall, Garden City Mall, Owerri Mall, Abia Mall, Enugu Mall, Onitsha Mall to the new ERP.
+	     <div className='boldy' > Shoprite Asaba Mall is scheduled to go live in:</div></p>
 			<div className='content'>
 				{Object.entries(timeLeft).map((el) => {
 					const label = el[0];
